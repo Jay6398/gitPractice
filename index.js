29c80c03fa48c2d9a6d316f7724c6928cfdd2788
Jay Shah <jayshah6398@gmail.com>
2022-08-02 18:54:41 +0530

    2nd

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const http = require("http");
 
 http
-  .createServer(function (req, res) {
+  .createServer(function (res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Hello World!");
+    console.log("herrr");
   })
   .listen(8080);
