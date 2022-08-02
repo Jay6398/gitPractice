0855094865b0bec74b03db6aebb0836cf57aceca
Jay Shah <jayshah6398@gmail.com>
2022-08-02 18:57:16 +0530

    3rdnew

---
+++ hello.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+http
+  .createServer(function (res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World!");
+    console.log("herrr");
+  })
+  .listen(5000);
